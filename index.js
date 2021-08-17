543f3951a99aa44487e10062aced83e61f839d35
NBender8668 <61422838+NBender8668@users.noreply.github.com>
2021-08-17 08:29:46 -0600

    here ya go dalton

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-//Hello there
-//wow
+const express = require('express');
+const pug = require('pug');
+const routes = require('./routes/routes');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+let urlencodedparser = express.urlencoded(
+    {
+        extended: false
+    }
+    );
+
+app.get('/', routes.index);
+
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb+srv://NoahBender:-3C.Uczz7bEii@g@cluster0.5ab23.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error'));
+mdb.once('open', callback => {});
+
+exports.index = (req, res) =>
+{
+    if(err) return console.error(err);
+    res.render('index', {
+        
+    });
+};
+
+// Joke.find(req.query.category ? {Category: req.query.category} : {}, (err, joke) => 
+// {
+//     if(err) return console.error(err);
+//     console.log(joke);
+//     res.render('index',
+//     {
+        
+//     });
+// }).limit(req.query.amount ? parseInt(req.query.amount) : 1000);
